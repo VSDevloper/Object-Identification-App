db615f8b44d60343af66daf2745c0f94d0703228
VSDevloper <84179803+VSDevloper@users.noreply.github.com>
2021-10-27 19:23:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,7 +10,10 @@ function preload(){
 
 
 function setup() {
-  canvas = createCanvas(640, 420);
+  canvas = createCanvas(380, 380);
+  video = createCapture(VIDEO);
+  video.size(380,380);
+  video.hide();
   canvas.center();
   objectDetector = ml5.objectDetector('cocossd', modelLoaded);
   document.getElementById("status").innerHTML = "Status : Detecting Objects";
@@ -19,7 +22,7 @@ function setup() {
 function modelLoaded() {
   console.log("Model Loaded!")
   status = true;
-  objectDetector.detect(img, gotResult);
+  objectDetector.detect(video, gotResult);
 }
 
 function gotResult(error, results) {
@@ -32,18 +35,23 @@ function gotResult(error, results) {
 
 
 function draw() {
-  image(img, 0, 0, 640, 420);
+  image(video, 0, 0, 380, 380);
 
       if(status != "")
       {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video, gotResult);
         for (var i = 0; i < objects.length; i++) {
           document.getElementById("status").innerHTML = "Status : Object Detected";
+          document.getElementById("number_of_objects").innerHTML = "Number of objects :" + objects.length;
     
-          fill(255, 0, 0);
+          fill(r,g,b);
           percent = floor(objects[i].confidence * 100);
           text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
           noFill();
-          stroke(255, 0, 0);
+          stroke(r,g,b);
           rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
         }
       }
